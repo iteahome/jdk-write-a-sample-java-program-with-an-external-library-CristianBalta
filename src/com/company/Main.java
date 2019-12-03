75f485ca66c1c2ee2662f9d026dc6744ff2c7ae8
CristianBalta <57498398+CristianBalta@users.noreply.github.com>
2019-12-03 17:16:28 +0200

    hello

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import org.joda.time.DateTime;
+
+public class Main
+{
+
+
+    public static void main(String[] args) {
+
+        DateTime now = new DateTime();
+        System.out.println(now.toDateMidnight());
+
+
+    }
+
+}
+
+
